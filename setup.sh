f438bfb8d6d344e13541cbcea5de49fc78cf4ac5
Rahul Yedida <rahul.yedida@pm.me>
2020-10-17 03:50:00 -0400

    Bug fixes

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ want=$Var/vimrc;
 set nocompatible              " be iMproved, required
 set mouse=a
 set shell=/bin/bash
+set splitbelow
 filetype off                  " required
 " set the runtime path to include Vundle and initialize
 set rtp+=~/.vim/bundle/Vundle.vim
@@ -61,6 +62,24 @@ autocmd FileType python noremap <buffer> <F8> :call Autopep8()<CR>
 " see :h vundle for more details or wiki for FAQ
 " Put your non-Plugin stuff after this line
 
+"Use 24-bit (true-color) mode in Vim/Neovim when outside tmux.
+"If you're using tmux version 2.2 or later, you can remove the outermost $TMUX check and use tmux's 24-bit color support
+"(see < http://sunaku.github.io/tmux-24bit-color.html#usage > for more information.)
+if (empty($TMUX))
+  if (has("nvim"))
+    "For Neovim 0.1.3 and 0.1.4 < https://github.com/neovim/neovim/pull/2198 >
+    let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+  endif
+  "For Neovim > 0.1.5 and Vim > patch 7.4.1799 < https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162 >
+  "Based on Vim patch 7.4.1770 (`guicolors` option) < https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd >
+  " < https://github.com/neovim/neovim/wiki/Following-HEAD#20160511 >
+  if (has("termguicolors"))
+    set termguicolors
+  endif
+endif
+
+syntax on
+colorscheme onedark
 
 set number autoindent
 syntax on
@@ -68,7 +87,16 @@ filetype plugin indent on
 set tabstop=4 shiftwidth=4 noexpandtab
 set noerrorbells
 set belloff=all
+
+set foldmethod=indent
+set foldnestmax=10
+set nofoldenable
+set foldlevel=2
 EOF
+
+mv $Var/vimrc ~/.vimrc
+cd ~/.vim/bundle/youcompleteme/ && python3 install.py
+git clone https://github.com/joshdick/onedark.vim.git ~/.vim/pack/*/opt/onedark.vim/
 
 if [ ! -d "$HOME/.vim/bundle" ]; then
    git clone https://github.com/VundleVim/Vundle.vim.git \
@@ -261,3 +289,4 @@ alias gcloud=/Users/ryedida/Downloads/google-cloud-sdk/bin/gcloud
 alias jupyter=/Users/ryedida/opt/anaconda3/bin/jupyter
 EOF
 
+mv $Var/.tmuxrc ~/.tmuxrc
